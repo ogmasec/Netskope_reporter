100c31020f482ced375c7d7e73d40c233d37083c
Mathieu Guerin <mathieu.guerin@ogmasec.fr>
2023-08-29 13:25:38 +0200

    Update main.py
    API getting data

--- main.py
+++ main.py
@@ -6,17 +6,46 @@
 ##### Import requests
 import private.config as config
 from modules.logger import *
+import time
+import requests
 #####
 
-if __name__ == '__main__':
-    
-    test = "192.168.1.1","user","443"
-    logger.debug("test %s",test)
-    logger.info("test")
-    logger.warning("test")
-    logger.error("test")
-    logger.critical("test")
 
+class NetskopeAPI:
+    def __init__(self, base_url, api_token):
+        self.base_url = base_url
+        self.api_token = api_token
 
+    def make_request(self, endpoint, method="GET", params=None, data=None):
+        url = self.base_url + endpoint
 
+        headers = {
+            "Netskope-Api-Token": self.api_token,
+            "Content-Type": "application/json"
+        }
 
+        response = requests.request(
+            method=method,
+            url=url,
+            headers=headers,
+            params=params,
+            data=data
+        )
+
+        return response
+
+# Utilisation de la classe NetskopeAPI
+if __name__ == "__main__":
+    api_tenant = config.parse['params']['NSKP_TENANT_HOSTNAME'] 
+    api_token = config.parse['params']['NSKP_TOKEN'] 
+    api_url = config.parse['params']['NSKP_DLP'] 
+    api_base_url = api_tenant+api_url
+    api_client = NetskopeAPI(api_base_url, api_token)
+
+    response = api_client.make_request("", params={"operation": "head", "index": "reporter"})
+
+    if response.status_code == 200:
+        data = response.json()
+        print("API Response:", data)
+    else:
+        print("Error:", response.status_code)
