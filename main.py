9078f8847178b471433b46c5109dba09b78d67c5
Mathieu Guerin <mathieu.guerin@ogmasec.fr>
2023-12-20 15:01:29 +0100

    Adding iterator loop

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ import private.config as config
 from modules.logger import *
 #from modules.pdfGen import *
 from modules.htmlGen import *
+from datetime import datetime
 import requests
-from time import sleep
-import requests
+import time
 import pandas as pd
 import json
 #####
@@ -22,20 +22,23 @@ class NetskopeAPI:
         self.api_token = config.parse['params']['NSKP_TOKEN'].lower()
         self.api_url = config.parse['params']['NSKP_URL'].lower()
         self.url = self.api_tenant+self.api_url
+        self.session = requests.Session()
         logger.info(self)
 
     def __str__(self):
-        return f"Instanciation of object for tenant {self.url}..."
+        return f"Instanciation of object for tenant {self.url} with session {self.session}..."
 
-    def _request(self,endpoint, method="GET", params=None,  data=None):
+    def _request(self,endpoint, params=None,  data=None):
         url = self.url + endpoint #endpoint is refered to the swagger
         logger.info(f"Endpoint: {url}?{params}")
         headers = {
             "Netskope-Api-Token": self.api_token,
             "Content-Type": "application/json"
         }
-        response = requests.request(
-            method=method,
+        
+        #response = requests.request(
+        response = self.session.get(
+            #method=method,
             url=url,
             headers=headers,
             params=params,
@@ -45,8 +48,11 @@ class NetskopeAPI:
     
     def get_dlp_incident(self,params_dlp,wait=5):
         # FAR : change the way off passing parameters
-        dlp_response = self._request(config.parse['params']['NSKP_ALERTS_DLP'],params=f"operation={params_dlp['operation']}&index={params_dlp['index']}")
-        
+        dlp_response = self._request(
+            config.parse['params']['NSKP_ALERTS_DLP'],
+            params=f"operation={params_dlp['operation']}&index={params_dlp['index']}",
+
+            )
         if dlp_response is not None:
             try:
                 dlp_response.raise_for_status()  # Check status
@@ -58,31 +64,36 @@ class NetskopeAPI:
         else:
             logger.error(f"Error occurred during request: {dlp_response}.")
             print(f"Error occurred during request: {dlp_response}.")
-        time.sleep(wait)
+        #time.sleep(wait)
         return data
 
 # Utilisation de la classe NetskopeAPI
 if __name__ == "__main__":
     
     ### Get DLP Incidents ###
-    nskp_dlp_incidents = NetskopeAPI()
-    today = int(time.time())
     #FAR : Get args
-    start = today-864000 # -10 days
+    nskp_dlp_incidents = NetskopeAPI()
+    now = int(time.time())
+    start = now-864000 # -10 days
+    #today = 1702631232 ## testing from
+    pause = 5
     last = 0
-    
-    params_dlp = {"operation": str(start),"index":"PYSCRIPT_TAM_"+str(today)}
-    dlp_incidents_list = nskp_dlp_incidents.get_dlp_incident(params_dlp)
-    pause = dlp_incidents_list.get('wait_time')
-    
-    params_dlp = {"operation": "next","index":"PYSCRIPT_TAM"}
-    """
-    while last+3600 < today:
+
+    logger.info(f"start date :{datetime.fromtimestamp(start)} stop date: {datetime.fromtimestamp(now)}")
+    operation = start
+    while last+3600 < now:
+        params_dlp = {"operation": operation,"index":"PYSCRIPT_TAM_"+str(now)}
+        dlp_incidents_list = nskp_dlp_incidents.get_dlp_incident(params_dlp,wait=pause)
+        last = dlp_incidents_list.get('timestamp_hwm')
+        pause = dlp_incidents_list.get('wait_time')
         if 'result' in dlp_incidents_list and isinstance(dlp_incidents_list['result'], list) and len(dlp_incidents_list['result']) != 0:
-            dlp_incidents_list = nskp_dlp_incidents.get_dlp_incident(params_dlp,wait=pause)
-            last = dlp_incidents_list.get('timestamp_hwm')
-            pause = dlp_incidents_list.get('wait_time')
-    """
+            print(dlp_incidents_list)
+            
+        if operation != "next": ## change "opearion" from "head" to next
+            operation = "next"
+            logger.info(f"Operation is now 'Next'...")
+    
+    logger.info(f"Duration = {int(time.time())-now} secondes")
 
 
 
